2a22e3c40d2bb1591f735a72de89d72513548690
Raymond John Grice <raymondjgrice@gmail.com>
2016-01-14 18:04:10 -0800

    Background work

---
+++ mp_shump/Assets/textureScroll.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textureScroll : MonoBehaviour
+{
+    
+    public float uvAnimationRate;
+    public float uvYPos;
+
+    private int materialIndex = 0;
+    private string textureName = "_MainTex";
+
+    private Vector2 uvOffset;
+    private Renderer render;
+
+    void Awake()
+    {
+        uvOffset = new Vector2(0, uvYPos);
+        render = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        uvOffset.x += uvAnimationRate * Time.deltaTime;
+        if (render.enabled)
+        {
+            render.material.SetTextureOffset(textureName, uvOffset);
+        }
+    }
+}
